951b101b32fda1011ff00fe3ea8a3804c24f6e15
aiXing <862099730@qq.com>
2016-03-15 14:22:17 +0800

    First commit.

---
+++ AXCollectionViewFlowLayout/AXCollectionViewFlowLayout/AXCollectionViewFlowLayout.m
@@ -0,0 +1,71 @@
+//
+//  AXCollectionViewFlowLayout.m
+//  AXCollectionViewFlowLayout
+//
+//  Created by ai on 16/3/15.
+//  Copyright © 2016年 devedbox. All rights reserved.
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
+
+#import "AXCollectionViewFlowLayout.h"
+
+@implementation AXCollectionViewFlowLayout
+#pragma mark - Override
+- (BOOL)shouldInvalidateLayoutForBoundsChange:(CGRect)oldBounds
+{
+    return YES;
+}
+
+- (NSArray<UICollectionViewLayoutAttributes *> *)layoutAttributesForElementsInRect:(CGRect)rect {
+    NSArray* array = [super layoutAttributesForElementsInRect:rect];
+    CGRect visibleRect;
+    visibleRect.origin = self.collectionView.contentOffset;
+    visibleRect.size = self.collectionView.bounds.size;
+    
+    for (UICollectionViewLayoutAttributes* attributes in array) {
+        if (CGRectIntersectsRect(attributes.frame, rect)) {
+            CGFloat distance = CGRectGetMidX(visibleRect) - attributes.center.x;
+            CGFloat normalizedDistance = distance / attributes.size.width;
+            if (ABS(distance) < attributes.size.width) {
+                CGFloat zoom = 1 - 0.06 * ABS(normalizedDistance);
+                attributes.transform3D = CATransform3DMakeScale(zoom, zoom, 1.0);
+                attributes.zIndex = 1;
+            }
+        }
+    }
+    return array;
+}
+
+- (CGPoint)targetContentOffsetForProposedContentOffset:(CGPoint)proposedContentOffset withScrollingVelocity:(CGPoint)velocity
+{
+    CGFloat offsetAdjustment = MAXFLOAT;
+    CGFloat horizontalCenter = proposedContentOffset.x + (CGRectGetWidth(self.collectionView.bounds) / 2.0);
+    
+    CGRect targetRect = CGRectMake(proposedContentOffset.x, 0.0, self.collectionView.bounds.size.width, self.collectionView.bounds.size.height);
+    NSArray* array = [super layoutAttributesForElementsInRect:targetRect];
+    
+    for (UICollectionViewLayoutAttributes* layoutAttributes in array) {
+        CGFloat itemHorizontalCenter = layoutAttributes.center.x;
+        if (ABS(itemHorizontalCenter - horizontalCenter) < ABS(offsetAdjustment)) {
+            offsetAdjustment = itemHorizontalCenter - horizontalCenter;
+        }
+    }
+    return CGPointMake(proposedContentOffset.x + offsetAdjustment, proposedContentOffset.y);
+}
+@end
---
+++ AXCollectionViewFlowLayout/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AXCollectionViewFlowLayout
+//
+//  Created by ai on 16/3/15.
+//  Copyright © 2016年 devedbox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
