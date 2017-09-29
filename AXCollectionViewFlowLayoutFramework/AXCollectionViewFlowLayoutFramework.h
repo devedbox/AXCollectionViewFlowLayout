845870118550afca32759e3c5c61a757500edf1a
aixing-hb <aixing@hydsoft.com>
2017-09-29 15:50:50 +0800

    Added carthage framework target.

---
+++ AXCollectionViewFlowLayoutFramework/AXCollectionViewFlowLayoutFramework.h
@@ -0,0 +1,19 @@
+//
+//  AXCollectionViewFlowLayoutFramework.h
+//  AXCollectionViewFlowLayoutFramework
+//
+//  Created by devedbox on 2017/9/29.
+//  Copyright © 2017年 devedbox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AXCollectionViewFlowLayoutFramework.
+FOUNDATION_EXPORT double AXCollectionViewFlowLayoutFrameworkVersionNumber;
+
+//! Project version string for AXCollectionViewFlowLayoutFramework.
+FOUNDATION_EXPORT const unsigned char AXCollectionViewFlowLayoutFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AXCollectionViewFlowLayoutFramework/PublicHeader.h>
+
+
